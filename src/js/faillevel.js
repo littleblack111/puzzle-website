b6ae19f8673d45a26895f5e508a37816b0ef9b08
littleblack111 <littleblack11111@gmail.com>
2023-11-05 23:00:14 +0800

    init

---
+++ src/js/faillevel.js
@@ -0,0 +1,24 @@
+function failLevel() {
+	let dialog = document.querySelector('[greetdialog]');
+
+	dialog.innerHTML = `
+	<dialog style="z-index: 1000;" id="dialog">
+		<h2>Bruhhhhh, You Failed!!!</h2>
+		<p>You've Failed the ${dialog.getAttribute('page')} challenge!</p>
+		<h4>Press the button below to go to the next level!!!</h4>
+		<form method="dialog">
+			<button autofocus id="tryAgain">Try Again</button>
+			<button id="finishHomePressed">Go Home</button>
+		</form>
+	</dialog>  
+	`
+
+	document.querySelector('#dialog').showModal();open
+
+	document.querySelector('#finishHomePressed').addEventListener('click', () => {
+		window.location.href = '/'
+	})
+	document.querySelector('#tryAgain').addEventListener('click', () => {
+		window.location.reload()
+	})
+}
