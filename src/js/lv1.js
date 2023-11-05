660b13db0675a562268c1e56237cba3c02979fa3
littleblack111 <littleblack11111@gmail.com>
2023-11-05 22:04:55 +0800

    made pointer obv

--- src/js/lv1.js
+++ src/js/lv1.js
@@ -1,4 +1,5 @@
 window.onload = function() {
 	const correct = document.getElementById(Math.floor(Math.random() * 12) + 1 + "tryme")
 	correct.href = "javascript:finishLevel(nextlv = '/lv2')";
+	correct.style.cursor = "pointer";
 }
