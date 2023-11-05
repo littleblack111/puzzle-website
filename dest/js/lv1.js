e350f4278b2522c7a41582f2d64d26902b313362
littleblack111 <littleblack11111@gmail.com>
2023-11-05 23:06:09 +0800

    give up on dest

--- dest/js/lv1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-window.onload = function () {
-  var correct = document.getElementById(Math.floor(Math.random() * 12) + 1 + "tryme");
-  correct.href = "javascript:finishLevel(nextlv = '/lv2')";
-  correct.style.cursor = "pointer";
-};
