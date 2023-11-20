7cd586fa00d15eea45c36ee6a44f3e81b606ec7c
littleblack111 <littleblack11111@gmail.com>
2023-11-20 16:33:54 +0800

    added hint to check console for flag

--- src/js/finishlevel.js
+++ src/js/finishlevel.js
@@ -5,6 +5,7 @@ function finishLevel() {
 	<dialog style="z-index: 1000;" id="dialog">
 		<h2>Yoooo! Congratulations!!!</h2>
 		<p>You've completed the ${dialog.getAttribute('page')} challenge!</p>
+		<h4>Check the Console for flag</h4>
 		<h4>Press the button below to go to the next level!!!</h4>
 		<form method="dialog">
 			<button autofocus id="finishPressed">Let's Go!</button>
