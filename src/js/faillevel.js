d79fff9b334b7ac9f3bc4749b1590ef4ba04edcf
littleblack111 <littleblack11111@gmail.com>
2023-11-23 08:52:39 +0800

    updated text from go to next level -> retry

--- src/js/faillevel.js
+++ src/js/faillevel.js
@@ -5,7 +5,7 @@ function failLevel() {
 	<dialog style="z-index: 1000;" id="dialog">
 		<h2>Bruhhhhh, You Failed!!!</h2>
 		<p>You've Failed the ${dialog.getAttribute('page')} challenge!</p>
-		<h4>Press the button below to go to the next level!!!</h4>
+		<h4>Press the button below to retry!!!</h4>
 		<form method="dialog">
 			<button autofocus id="tryAgain">Try Again</button>
 			<button id="finishHomePressed">Go Home</button>
